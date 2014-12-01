4596720952702cc2ac65162d6e2607bc5ba5d605
jgazhenko3 <gazhenko@gatech.edu>
2014-12-01 08:10:54 -0500

    formatting

--- memcached_asm/run.sh
+++ memcached_asm/run.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+###############################################################################
+# To run a hash function, uncomment the block of code that you would like to
+# run.
+# 
+# Author: Jemmy Gazhenko
+# Date: Dec. 1 2014
+# Version: 1
+###############################################################################
+
 #echo 'Testing Jenkins Hash Assembly'
 #echo '-----------------------------'
 
@@ -19,15 +28,15 @@
 #harptool -L -o murmur_hash.HOF boot.out lib.out murmur_hash.out
 #harptool -E -c murmur_hash.HOF
 
-echo 'Testing Jenkins Hash Assembly library'
-echo '-----------------------------'
+#echo 'Testing Jenkins Hash Assembly library'
+#echo '-----------------------------'
 
-harptool -A -o boot.out boot.s
-harptool -A -o lib.out lib.s
-harptool -A -o jenkins_hash_lib.out jenkins_hash_lib.s
-harptool -A -o jenkins_hash_program.out jenkins_hash_program.s
-harptool -L -o jenkins_hash_program.HOF boot.out lib.out jenkins_hash_lib.out jenkins_hash_program.out
-harptool -E -c jenkins_hash_program.HOF
+#harptool -A -o boot.out boot.s
+#harptool -A -o lib.out lib.s
+#harptool -A -o jenkins_hash_lib.out jenkins_hash_lib.s
+#harptool -A -o jenkins_hash_program.out jenkins_hash_program.s
+#harptool -L -o jenkins_hash_program.HOF boot.out lib.out jenkins_hash_lib.out jenkins_hash_program.out
+#harptool -E -c jenkins_hash_program.HOF
 
 #echo 'Testing Murmur Hash Assembly library'
 #echo '-----------------------------'
